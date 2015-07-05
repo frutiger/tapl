132575a21d551dceddc2deabe24ec3eb663306a3
Masud Rahman <masud.s.rahman@gmail.com>
2015-07-05 15:12:52 -0400

    Introduces 'driver'.
    The 'driver' module will be capable of launching arbitrary interpreters.

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(name='tapl',
+      version='0.1',
+      description='Implementations from Types and Programming Languages',
+      url='https://github.com/frutiger/tapl',
+      author='Masud Rahman',
+      license='MIT',
+      packages=['tapl'],
+      entry_points={
+      })
+
